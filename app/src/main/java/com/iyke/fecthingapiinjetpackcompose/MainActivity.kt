3e59ba528e3551dc29015110287d21541e6e969b
emmanueldav <kuradavis987@gmail.com>
2022-08-11 14:41:13 +0100

    added expandable card

--- app/src/main/java/com/iyke/fecthingapiinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/iyke/fecthingapiinjetpackcompose/MainActivity.kt
@@ -3,13 +3,27 @@ package com.iyke.fecthingapiinjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.iyke.fecthingapiinjetpackcompose.ui.theme.FecthingApiInJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,13 +45,109 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row {
+                        Text("Joke Api ")
+                    }
+                },
+            )
+        },
+        content = {
+           Column {
+               Text(text = "Jokes")
+
+           }
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FecthingApiInJetpackComposeTheme {
-        Greeting("Android")
+     ExpandableCard(header = "b j edcj ", description ="jcjscbjbb", color = Color.Red)
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ExpandableCard(
+    header: String, // Header
+    description: String, // Description
+    color: Color, // Color
+) {
+    var expand by remember { mutableStateOf(false) } // Expand State
+    val rotationState by animateFloatAsState(if (expand) 180f else 0f) // Rotation State
+    var stroke by remember { mutableStateOf(1) } // Stroke State
+
+    Card(
+        modifier = Modifier
+            .animateContentSize( // Animation
+                animationSpec = tween(
+                    durationMillis = 400, // Animation Speed
+                    easing = LinearOutSlowInEasing // Animation Type
+                )
+            )
+            .padding(8.dp),
+        backgroundColor = Color.White,
+        shape = RoundedCornerShape(8.dp), // Shape
+        border = BorderStroke(stroke.dp, color), // Stroke Width and Color
+        onClick = {
+            expand = !expand
+            stroke = if (expand) 2 else 1
+        }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween // Control the header Alignment over here.
+            ) {
+                Text(
+                    text = header,
+                    color = color, // Header Color
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Start,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier
+                        .weight(.9f)
+                        .padding(start = 8.dp)
+                )
+                IconButton(
+                    modifier = Modifier
+                        .rotate(rotationState)
+                        .weight(.1f),
+                    onClick = {
+                        expand = !expand
+                        stroke = if (expand) 2 else 1
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.KeyboardArrowDown,
+                        tint = color, // Icon Color
+                        contentDescription = "Drop Down Arrow"
+                    )
+                }
+            }
+            if (expand) {
+                Text(
+                    text = description,
+                    color = color, // Description Color
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Start,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                )
+            }
+        }
     }
 }
